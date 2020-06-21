61867fd7f8235035e13db4c636538e553f65f1d8
Lizett Rincon <lrincon34@gmail.com>
2020-06-21 16:45:11 -0600

    Adding Analysis file.

---
+++ Python_practice.py
@@ -0,0 +1,14 @@
+temperature = int(input("What is the temperature outside?"))
+
+if temperature > 80:
+    print("Turn on the AC.")
+else
+    print("Open the windows.")
+
+counties = ["Araphone", "Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else
+    print("El Paso is not in the list of counties") 
+  
+       
